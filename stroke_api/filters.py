b079638bd80b1aa5b80029b972d54da6dfbf61d3
samyachd <achd.samy@gmail.com>
2025-08-08 14:10:25 +0200

    changement

--- stroke_api/filters.py
+++ stroke_api/filters.py
@@ -1,43 +1,66 @@
-#from typing import Optional
+from typing import Optional
+from fastapi import HTTPException
+import plotly_express as px
+import streamlit as st
 import pandas as pd
+from enum import Enum
 
 # Chargement des données (une fois)
-stroke_data_df = pd.read_parquet("../stroke-api/data/healthcare-dataset-stroke-data-clean.parquet")
 
 # Tester l'app avec :
-#poetry run fastapi dev stroke_api/main.py
+# poetry run fastapi dev stroke_api/main.py
 # http://127.0.0.1:8000/docs : utiliser la fonctionnalité Try it out pour tester les routes
 
 # Ajout des fonctions de filtrage des données cf notebook 1
-
-def filter_patient(gender: str = None, stroke: int = None, max_age: float = None):
-
-    filter_df = stroke_data_df.copy()
-
-    if gender is not None:
-        filter_df = filter_df[filter_df['gender'] == gender]
-
-    if stroke is not None:
-        filter_df = filter_df[filter_df['stroke'] == stroke]
-
-    if max_age is not None:
-        filter_df = filter_df[filter_df['age'] <= max_age]
-
-    return filter_df.to_dict('records')
-
 # Ensuite faire appel à ces fonctions dans le fichier api.py où sont définies les routes.
 
 # Ajouter les fonctions de filtrage pour les autres routes.
 
-def get_patients_by_id(patient_id: int = None):
+stroke_data_df = pd.read_parquet("../stroke-api/data/healthcare-dataset-stroke-data-clean.parquet")
+
+class StatsName(str, Enum):
+    number_stat = "nombre"
+    age_moyen_stat = "age moyen"
+    taux_avc_stat = "taux d'avc"
+    all = "All"
+
+def filter_patient(gender:Optional[str] = None, stroke:Optional[bool] = None, max_age:Optional[float] = None):
+    df = stroke_data_df.copy()
+
+    if max_age is not None:
+        df= df[df["age"] <= max_age]
+
+    if gender is not None:
+        df = df[df["gender"] == gender]
+
+    if stroke is not None:
+        df = df[df["stroke"] == stroke]
+
+    return df.to_dict('records')
+
+def read_patient(patient_id:int):
+    return {"patient_id":patient_id}
+
+def filter_id(id_patient:int):
+    df = stroke_data_df.copy()
     
-    filter_df = stroke_data_df.copy()
+    if id_patient not in df["id"].tolist():
+        raise HTTPException(status_code=404, detail="Patient not found")
+    return df.loc[df["id"] == id_patient].to_dict('records')
+    
+def stats(type: StatsName = None):
+# nb total de patients, âge moyen, taux d’AVC, répartition hommes/femmes
+    df = stroke_data_df.copy()
 
-    patient = filter_df.loc[filter_df["id"] == patient_id]
-
-    if patient.empty:
-        return None
-
-    return patient.to_dict('records')
-
-
+    nb_patient = df["id"].count()
+    age_moyen =  df["age"].mean()
+    avc_moyen = df["stroke"].mean()
+    
+    if type == StatsName.number_stat:
+        return {f"{round(nb_patient)} patients"}
+    elif type == StatsName.age_moyen_stat:
+        return {f"{round(age_moyen)} ans en moyenne"}
+    elif type == StatsName.taux_avc_stat:
+        return {f"{avc_moyen}% taux d'avc"}
+    elif type == StatsName.all:
+        return {f"{round(nb_patient)} patients, ", f"{round(age_moyen)} ans en moyenne, ", f"{avc_moyen}% taux d'avc"}
